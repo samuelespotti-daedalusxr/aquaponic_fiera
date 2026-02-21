85127b096f3a504e237fe2ef22466613087ced53
samuelespotti-daedalusxr <samuele.spotti@daedalusxr.it>
2026-02-21 17:35:21 +0100

    Update App.tsx with new component structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,32 @@
+import { LanguageProvider } from './context/LanguageContext';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
-import { Exhibitors } from './components/Exhibitors';
-import { Workshops } from './components/Workshops';
-import { RoundTables } from './components/RoundTables';
-import { EventInfo } from './components/EventInfo';
-import { CTA } from './components/CTA';
+import { Highlights } from './components/Highlights';
+import { Program } from './components/Program';
+import { Formats } from './components/Formats';
+import { Institutional } from './components/Institutional';
+import { Venue } from './components/Venue';
+import { Partners } from './components/Partners';
+import { NewsletterForm } from './components/NewsletterForm';
+import { FAQ } from './components/FAQ';
 import { Footer } from './components/Footer';
 
 export default function App() {
   return (
-    <div className="min-h-screen">
-      <Hero />
-      <About />
-      <Exhibitors />
-      <Workshops />
-      <RoundTables />
-      <EventInfo />
-      <CTA />
-      <Footer />
-    </div>
+    <LanguageProvider>
+      <div className="min-h-screen" style={{ background: '#F6F3EC', color: '#1C1F1D', fontFamily: 'Inter, sans-serif' }}>
+        <Hero />
+        <About />
+        <Highlights />
+        <Program />
+        <Formats />
+        <Institutional />
+        <Venue />
+        <Partners />
+        <NewsletterForm />
+        <FAQ />
+        <Footer />
+      </div>
+    </LanguageProvider>
   );
 }
